1bc9650f7ca19ea47fcf5fbae35a3ee0f4d8f4c7
AF1QUE <afique.dev@gmail.com>
2022-02-22 21:31:33 +0600

    Finihing off

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 // Mobile hamburger toggle
 
-const navBlock = document.querySelector(".nav__block");
+const nav = document.querySelector(".nav");
 
 const navIcon = document.querySelector(".nav__icon");
 let flag = 0;
 navIcon.addEventListener("click", () => {
   if (flag % 2 === 0) {
     navIcon.src = "images/icon-close.svg";
-    navBlock.style.display = "block";
+    nav.style.display = "block";
     flag++;
   } else {
     navIcon.src = "images/icon-hamburger.svg";
-    navBlock.style.display = "none";
+    nav.style.display = "none";
     flag++;
   }
   console.log(flag);
 });
+
+// Inner DropDown Toggles
+
+const dropDown = document.querySelectorAll(".mobile-header .dropdown");
+
+for (let i = 0; i < dropDown.length; i++) {
+  dropDown[i].addEventListener("click", (e) => {
+    dropDown[i].classList.toggle("active");
+    dropDown[i].nextElementSibling.classList.toggle("active--inner");
+  });
+}
